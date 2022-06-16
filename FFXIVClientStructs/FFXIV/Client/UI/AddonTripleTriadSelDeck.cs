91dc0caf9a532453438cb9d976f9d6033393cc07
marimelon <29140293+marimelon@users.noreply.github.com>
2022-06-16 17:03:58 +0900

    Add AddonTripleTriadSelDeck

---
+++ FFXIVClientStructs/FFXIV/Client/UI/AddonTripleTriadSelDeck.cs
@@ -0,0 +1,15 @@
+﻿using FFXIVClientStructs.FFXIV.Component.GUI;
+
+namespace FFXIVClientStructs.FFXIV.Client.UI;
+
+// Client::UI::TripleTriadSelDeck
+//   Component::GUI::AtkUnitBase
+//     Component::GUI::AtkEventListener
+[StructLayout(LayoutKind.Explicit, Size = 0x230)]
+public unsafe struct AddonTripleTriadSelDeck
+{
+    [FieldOffset(0x0)] public AtkUnitBase AtkUnitBase;
+
+    [FieldOffset(0x220)] public AtkComponentList* AtkComponentList220;
+    [FieldOffset(0x228)] public AtkTextNode* AtkTextNode228;
+}
