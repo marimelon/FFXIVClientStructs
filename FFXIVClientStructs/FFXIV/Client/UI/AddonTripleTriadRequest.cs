7a56449f933053b0f5fa8ab58c21ab3011e16d15
marimelon <29140293+marimelon@users.noreply.github.com>
2025-05-06 00:46:10 +0900

    Add AddonTripleTriadRequest

---
+++ FFXIVClientStructs/FFXIV/Client/UI/AddonTripleTriadRequest.cs
@@ -0,0 +1,33 @@
+﻿using FFXIVClientStructs.FFXIV.Component.GUI;
+
+namespace FFXIVClientStructs.FFXIV.Client.UI;
+
+// Client::UI::AddonTripleTriadRequest
+//   Component::GUI::AtkUnitBase
+//     Component::GUI::AtkEventListener
+[StructLayout(LayoutKind.Explicit, Size = 0x2B0)]
+public unsafe struct AddonTripleTriadRequest
+{
+    [FieldOffset(0x0)] public AtkUnitBase AtkUnitBase;
+
+    [FieldOffset(0x220)] public AtkTextNode* AtkTextNode220;
+    [FieldOffset(0x228)] public AtkTextNode* AtkTextNode228;
+    [FieldOffset(0x230)] public AtkTextNode* AtkTextNode230;
+    [FieldOffset(0x238)] public AtkTextNode* AtkTextNode238;
+
+    [FieldOffset(0x240)] public AtkComponentDropDownList* AtkComponentDropDownList240;
+    [FieldOffset(0x248)] public AtkComponentDropDownList* AtkComponentDropDownList248;
+
+    [FieldOffset(0x250)] public AtkComponentBase* AtkComponentBase250;  // Regional Rule 1
+    [FieldOffset(0x258)] public AtkComponentBase* AtkComponentBase258;  // Regional Rule 2
+    [FieldOffset(0x260)] public AtkComponentBase* AtkComponentBase260;  // Match Rule 1
+    [FieldOffset(0x268)] public AtkComponentBase* AtkComponentBase268;  // Match Rule 2 
+
+    [FieldOffset(0x270)] public AtkResNode* MatchRulesResNode;
+
+    [FieldOffset(0x278)] public AtkComponentButton* ChallengeButton;
+    [FieldOffset(0x280)] public AtkComponentButton* QuitButton;
+    [FieldOffset(0x288)] public AtkComponentButton* RuleChangeButton;
+
+    [FieldOffset(0x2A8)] public AtkTextNode* AtkTextNode2A8;
+}
